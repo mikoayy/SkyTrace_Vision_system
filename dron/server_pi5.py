916284c152ccfa1dc35229b9440b4c21c98ee26a
mikoayy <mikolaj20031221@gmail.com>
2025-05-10 11:43:36 +0200

    Update and rename server.py to server_pi5.py

---
+++ dron/server_pi5.py
@@ -0,0 +1,25 @@
+from picamera2 import Picamera2
+import cv2
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+picam2 = Picamera2()
+config = picam2.create_preview_configuration(main={"format": "RGB888", "size": (640, 480)})
+picam2.configure(config)
+picam2.start()
+
+def generate_frame():
+    while True:
+        frame = picam2.capture_array()
+        _, buffer = cv2.imencode(".jpg", frame)
+        frame = buffer.tobytes()
+        yield (b"--frame\r\n"
+               b"Content-Type: image/jpeg\r\n\r\n" + frame + b"\r\n")
+
+@app.route("/")
+def video_feed():
+    return Response(generate_frame(), mimetype="multipart/x-mixed-replace; boundary=frame")
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, threaded=True)
